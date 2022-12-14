ad7e968e776df7d50547393522a70533c89b4243
Nycolas Felipe <nycolaslourenco.contato@gmail.com>
2022-12-13 21:34:12 -0300

    Update 2441_janela.py

--- beecrowd/2441_janela.py
+++ beecrowd/2441_janela.py
@@ -1,1 +1,25 @@
-folha_1, folha_2, folha_3 = map(int, input().split())
+f1, f2, f3 = map(int, input().split())
+folhas = [f1, f1+200, f2, f2+200, f3, (f3+200, 600)[f3+200 > 600]]
+
+for i in range(len(folhas)):
+  print(folhas[i])
+  # pegar indices pares 
+  # comparar o i com indice par e indice par-1
+  # e ver se ele esta entre os dois valores
+
+print(folhas)
+
+# largura = 0
+
+# for i in range(1, len(folhas)):
+#   distancia = 0
+#   if i != len(folhas)-1:
+#     distancia = folhas[i]-folhas[i-1]-200
+#     if distancia > 0:
+#       largura += distancia
+#   else:
+#     distancia = 600-folhas[i]-200
+#   if distancia > 0:
+#     largura += distancia
+
+# print(largura)
